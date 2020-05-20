c6b0a63ec457e1565603eeb4ecd4e69bfdb92ce0
cy <1337103725@qq.com>
2020-05-20 11:33:45 +0800

    ''

--- DS7_Graph/1.introduction.py
+++ DS7_Graph/1.introduction.py
@@ -103,6 +103,10 @@ class Graph:
     def getVertices():
         return self.vertList.keys()
 
+    # 添加一个迭代器
+    def __iter__(self):
+        return iter(self.vertList.values())
+
 
 g = Graph()
 for i in range(6):
@@ -110,5 +114,16 @@ for i in range(6):
 print(g.vertList)
 g.addEdge(0, 1, 5)
 g.addEdge(0, 5, 2)
+g.addEdge(1, 2, 4)
+g.addEdge(2, 3, 9)
+g.addEdge(3, 4, 7)
+g.addEdge(3, 5, 3)
+g.addEdge(4, 0, 1)
+g.addEdge(5, 4, 8)
+g.addEdge(5, 2, 1)
+
+for v in g:
+    for w in v.getConnections():
+        print("(%s,%s)" % (v.getId(), w.getId()))
 
 # 作业，测试以上ADT实现类
